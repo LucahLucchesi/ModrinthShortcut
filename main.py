e885a64bade074851f4bb147f87e8721450181e1
Lucah Lucchesi <lucahlucchesi@gmail.com>
2024-08-16 12:58:50 -0500

    Automatic download, fixed entity culling

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
+import webbrowser
 
 modrinth_url = 'https://api.modrinth.com/v2/project/'
 
 # all mods listed are client-side and do not impact the vanilla experience
 mod_list = {
     'fabric_api': 'P7dR8mSH',
-    'mod_menu': 'NNAgCjsB',
+    'mod_menu': 'mOgUt4GM',
     'sodium': 'AANobbMI', # general optimization
     'sodium_extra': 'PtjYWJkn', # extra sodium options
     'lithium': 'gvQqBUqZ', # tick optimization
@@ -21,6 +22,7 @@ def main():
             versions = requests.get(modrinth_url + mod_list[mod] + '/version').json()
             latest_ver = versions[0]
             print(mod + ": " + latest_ver['files'][0]['url'])
+            webbrowser.open(latest_ver['files'][0]['url'])
         except:
             print("Error: " + mod + " not found.")
 
