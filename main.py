24d9d71a4a9406ace1c12dd10122ed60895959a9
Lucah Lucchesi <lucahlucchesi@gmail.com>
2024-08-16 12:40:08 -0500

    Added more mods and exception handling

--- main.py
+++ main.py
@@ -2,18 +2,28 @@ import requests
 
 modrinth_url = 'https://api.modrinth.com/v2/project/'
 
-# dictionary of mod id's
+# all mods listed are client-side and do not impact the vanilla experience
 mod_list = {
     'fabric_api': 'P7dR8mSH',
-    'sodium': 'AANobbMI'
+    'mod_menu': 'NNAgCjsB',
+    'sodium': 'AANobbMI', # general optimization
+    'sodium_extra': 'PtjYWJkn', # extra sodium options
+    'lithium': 'gvQqBUqZ', # tick optimization
+    'ferrite_core': 'uXXizFIs', # memory optimization
+    'entity_culling': 'NNAgCjsB', # entity rendering optimization
+    'iris': 'YL57xq9U', # shaders
 }
 
 def main():
     # gets the version list for each mod and prints the download link for latest version
     for mod in mod_list:
-        versions = requests.get(modrinth_url + mod_list[mod] + '/version').json()
-        latest_ver = versions[0]
-        print(mod + ": " + latest_ver['files'][0]['url'])
+        try:
+            versions = requests.get(modrinth_url + mod_list[mod] + '/version').json()
+            latest_ver = versions[0]
+            print(mod + ": " + latest_ver['files'][0]['url'])
+        except:
+            print("Error: " + mod + " not found.")
+
 
 if __name__ == "__main__":
     main()
